003d431e521f64ab64833818d798ff7da109713f
Student User <student@workstation.lab.example.com>
2021-04-29 02:28:40 -0400

    Changed index page contents.

--- php-helloworld/index.php
+++ php-helloworld/index.php
@@ -3,4 +3,5 @@ print "Hello, World! php version is " . PHP_VERSION . "\n";
 print "A change is coming!\n";
 print "mohagungsolihin!\n";
 print "ini paling update\n";
+print "udah update lagi dong cooy\n";
 ?>
